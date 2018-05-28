280ef0d16b826d8729d51bf0a8ca85035ec84cf2
jumbycc@163.com <godcong>
2018-05-29 02:05:39 +0800

    an easy thread manage tool

--- thread_main.go
+++ thread_main.go
@@ -13,14 +13,24 @@ type ThreadMain struct {
 	transport sync.Pool
 }
 
+type NameAble interface {
+	Name() string
+}
+
 type ThreadAble interface {
 	Run(obj *ThreadMain) error
 	Name() string
 	ReceiveFrom(name string, val interface{}) error
 }
 
+type Thread interface {
+	RunServer(ctx context.Context)
+	Name() string
+	Stop()
+}
+
 type delayedTransmission struct {
-	Thread ThreadAble
+	Name NameAble
 	To     string
 	Val    interface{}
 }
@@ -35,8 +45,16 @@ func Register(t ThreadAble) () {
 	defaultThread.Register(t)
 }
 
-func SendTo(self ThreadAble, name string, val interface{}) {
+func RegisterThread(t Thread) () {
+	defaultThread.RegisterThread(t)
+}
+
+func SendTo(self NameAble, name string, val interface{}) {
 	defaultThread.SendTo(self, name, val)
+}
+
+func DelayedSendTo(self NameAble, name string, val interface{}){
+	defaultThread.DelayedSendTo(self,name,val)
 }
 
 //func Find(name string) (t ThreadAble) {
@@ -55,15 +73,15 @@ func (obj *ThreadMain) Register(t ThreadAble) {
 	obj.threads.Store(t.Name(), t)
 }
 
-func (obj *ThreadMain) DelayedSendTo(self ThreadAble, name string, val interface{}) {
+func (obj *ThreadMain) DelayedSendTo(self NameAble, name string, val interface{}) {
 	obj.transport.Put(&delayedTransmission{
-		Thread: self,
+		Name: self,
 		To:     name,
 		Val:    val,
 	})
 }
 
-func (obj *ThreadMain) SendTo(self ThreadAble, name string, val interface{}) {
+func (obj *ThreadMain) SendTo(self NameAble, name string, val interface{}) {
 	obj.threads.Range(
 		func(key, value interface{}) bool {
 			if v, b := value.(ThreadAble); b {
@@ -102,8 +120,14 @@ func (obj *ThreadMain) Start() {
 	defer cancel()
 	obj.threads.Range(
 		func(key, value interface{}) bool {
+			//run threadable
 			if v, b := value.(ThreadAble); b {
 				go obj.threadRun(ctx, v)
+				return true
+			}
+			//run thread
+			if v, b := value.(Thread); b {
+				go v.RunServer(ctx)
 				return true
 			}
 			log.Error("not succeeded with ", key)
@@ -119,12 +143,22 @@ func (obj *ThreadMain) Start() {
 		time.Sleep(time.Second)
 
 	}
+
+	//stop all threads
+	obj.threads.Range(
+		func(key, value interface{}) bool {
+			if v, b := value.(Thread); b {
+				v.Stop()
+			}
+			return true
+		})
+
 }
 
 func delayedSend(obj *ThreadMain) bool {
 	if v := obj.transport.Get(); v != nil {
 		if v0, b := v.(*delayedTransmission); b {
-			obj.SendTo(v0.Thread, v0.To, v0.Val)
+			obj.SendTo(v0.Name, v0.To, v0.Val)
 			return true
 		}
 	}
@@ -133,4 +167,8 @@ func delayedSend(obj *ThreadMain) bool {
 
 func (obj *ThreadMain) Stop() {
 	obj.stop = true
+}
+
+func (obj *ThreadMain) RegisterThread(thread Thread) {
+	obj.threads.Store(thread.Name(), thread)
 }
